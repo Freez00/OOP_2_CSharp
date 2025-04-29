3fb80342f08b783879a784ef8f732c949bff74a7
Михаил Жеков <114873472+Freez00@users.noreply.github.com>
2025-04-29 21:31:15 +0300

    Add files via upload

---
+++ 16042025/16042025/Program.cs
@@ -0,0 +1,152 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PointShowcase();
+    }
+
+    private static void BitWiseEven()
+    {
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Is even? - " + ((x & 1) == 0));
+
+    }
+
+    private static void BitwiseXORForArrayPairs()   //  A^B
+    {
+        int[] arr = {  1, 1, 1, 1, 2};
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            bool flag = false;
+            for(int j = 0; j < arr.Length; j++)
+            {
+                if (i == j)
+                    continue;
+
+                if ((arr[i] ^ arr[j]) == 0)
+                    flag = true;
+            }
+
+            if(!flag)
+                Console.WriteLine(arr[i]);
+        }
+    }
+
+    private static void BitCounter()
+    {
+        int x = 53; //  1 1 0 1 0 1
+        int count = 0;
+
+        while (x != 0)
+        {
+            if ((x & 1) == 1)
+                count++;
+
+            x = x >> 1;
+        }
+
+        Console.WriteLine(count);
+    }
+
+    private static void PointShowcase()
+    {
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point();
+
+        Console.WriteLine("p1 " + p1);
+        Console.WriteLine("p2 " + p2);
+
+        Console.WriteLine("d1 " + p1.DistanceTo(p2));
+        Console.WriteLine("d2 " + Point.DistanceBetween(p1, p2));
+
+    }
+
+}
+
+class Point
+{
+    private int _x;
+    private int _y;
+
+    public int x { get { return _x; } }
+    public int y { get { return _y; } }
+    public Point(int x, int y)
+    {
+        this._x = x;
+        this._y = y;
+    }
+
+    public Point() : this(0, 0) {}
+
+    public double DistanceTo(Point point)
+    {
+        double distance = Math.Sqrt(Math.Pow(this.x - point.x, 2) + Math.Pow(this.y - point.y, 2));
+
+        return Math.Round(distance,2);
+    }
+
+    public static double DistanceBetween(Point point1, Point point2)
+    {
+        double distance = Math.Sqrt(Math.Pow(point1.x - point2.x, 2) + Math.Pow(point1.y - point2.y, 2));
+
+        return Math.Round(distance, 2);
+    }
+
+    public override string ToString()
+    {
+        string s = $"[x] = {this.x}  |  [y] = {this.y}";
+        return s;
+    }
+}
+
+class Vehicle
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+
+    public int Year { get; set; }
+
+    public FuelType fuelType { get; set; }
+
+    public Vehicle(string brand, string model, int year, FuelType fuelType)
+    {
+        this.Brand = brand;
+        this.Model = model;
+        this.Year = year;
+        this.fuelType = fuelType;
+    }
+
+    public Vehicle() : this("", "", 0, FuelType.None) { }
+
+    public void Drive()
+    {
+        Console.WriteLine(this + " is driving");
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine(this);
+    }
+
+    public override string ToString()
+    {
+        return $"{Brand} {Model} {Year} [ft - {fuelType}]";
+    }
+
+
+}
+
+public enum FuelType
+{
+    None,
+    Petrol,
+    Diesel,
+    LPG,
+    Electricity,
+}
+
+class Car : Vehicle
+{
+
+}
---
+++ 23042025/23042025/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23042025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex1();
+            Ex2();
+
+        }
+        static void Ex1()
+        {
+            List<Student> students = new List<Student>();
+
+            //SeedStudentList(students);
+            //StudentsListToCSV(students);
+
+            CSVToStudentsList(students);
+            ViewList(students);
+        }
+        static void ViewList<T>(List<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void SeedStudentList(List<Student> students)
+        {
+            students.Add(new Student("Ivan", 19, 6));
+            students.Add(new Student("Petur", 18, 5));
+            students.Add(new Student("Kaloqn", 19, 3));
+            students.Add(new Student("Joro", 19, 4.5));
+            students.Add(new Student("Lubo", 19, 5.44));
+        }
+
+        static void StudentsListToCSV(List<Student> students)
+        {
+            string fileName = "data.txt";
+
+            FileStream fs = new FileStream(fileName, FileMode.Create);
+
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                foreach(Student s in students)
+                {
+                    writer.WriteLine(s.ToString());
+                }
+                fs.Flush();
+            }
+            fs.Dispose();
+        }
+
+        static void CSVToStudentsList(List<Student> students)
+        {
+            string fileName = "data.txt";
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] data = line.Split(',');
+                    students.Add(new Student(data[0], int.Parse(data[1]), double.Parse(data[2])));
+                }
+            }
+        }
+
+
+        static void Ex2()
+        {
+            #region path
+            string path = @"C:\Users\Freez_\Desktop\Software Engineering\PU\I kurs\OOP 2 - C#\23042025\23042025\keywords.txt";
+            #endregion
+            string keyword = Console.ReadLine();
+            var matches = FindMatches(keyword, path);
+            ViewList(matches);
+
+        }
+
+        static List<string> FindMatches(string keyword, string path)
+        {
+
+            List<string> list = new List<string>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                int lineIndex = 1;
+                while((line = sr.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ', ',', '.', '?', '!');
+                    
+                    foreach(string w in words)
+                    {
+                        if (w.StartsWith(keyword))
+                        {
+                            list.Add($"[{lineIndex}] - \"{w}\"");
+                        }
+                    }
+                    lineIndex++;
+                }
+            }
+            
+
+            return list;
+        }
+
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public double Grade { get; set; }
+
+        public Student(string name, int age, double grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+        public Student() : this("", 0, 0){}
+
+        public override string ToString()
+        {
+            return $"{Name},{Age},{Grade}";
+        }
+    }
+}
